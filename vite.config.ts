9017041c216dffbada43433a6815b7c647cfd0ce
Rénato TCHOBO <mariepotoate@gmail.com>
2026-01-02 10:25:02 +0100

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  // Cette ligne est cruciale pour que les chemins soient corrects après le build
+  base: './', 
+})
